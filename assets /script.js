1a0d8b7ed05614625a0077e936ebcbfdbb3b1a46
Kris10 <kmrichie611@gmail.com>
2023-09-05 12:02:13 -0500

    added pseudo code

--- assets /script.js
+++ assets /script.js
@@ -1,5 +1,20 @@
 // Assignment code here
 
+// Pseudocode:
+  //1. Understand problem: generate a random password that is at least 8 characters and no more than 128 characters and includes lower or upper case characters, numeric characters, and/or special characters. 
+
+  //2. When user clicks button, a series of prompts displays for password criteria: length and type of characters.
+
+  //3. When prompted for length of password, user chooses at least 8 characters for length for password, and no more than 128.
+
+  //4. When prompted for character type, user confirms whether or not to use lowercase, uppercase, numeric, and/or special characters.
+
+  //5. Computer randomly chooses character criteria for user when user answers yes or no to the type of characters. 
+
+  //6. Once user answers all prompts, computer randomly generates a password that matches the criteria selected by the user.
+
+  //7. Random password is displayed in an alert or written on the page. 
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
